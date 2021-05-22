537e73c4fc938c493aca9b86ebc18bde8850db3d
Kymani Emmanuel Glimeti <kymani20100@gmail.com>
2021-05-22 17:10:36 +0000

    I Love Code

--- App.js
+++ App.js
@@ -1,6 +1,12 @@
-import React from 'react';
-import {View, StyleSheet} from 'react-native';
+import * as React from 'react';
+import { Text, View, StyleSheet } from 'react-native';
 import Constants from 'expo-constants';
+
+// You can import from local files
+import AssetExample from './components/AssetExample';
+
+// or any pure javascript modules available in npm
+import { Card } from 'react-native-paper';
 
 import Header from './components/Header';
 export default function App() {
